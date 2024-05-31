4e447a5e56fa41bdf43e91519935c51562c6134d
m.lakhani.24471 <m.lakhani.24471@khi.iba.edu.pk>
2024-05-31 17:29:14 +0000

    Styling of theme

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:track_cash/utilities/theme.dart';
 import 'package:track_cash/widgets/google_signin_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,57 +9,77 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>{
-
+class _LoginScreenState extends State<LoginScreen> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Welcome,"),
-              const Text("Sign in to continue!"),
-              TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  filled: true,
-                  labelText: 'Username',
-                ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Welcome,",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Sign in to continue!",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  TextField(
+                    controller: _usernameController,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    decoration: textFieldTheme("Username", Icons.person, context),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: textFieldTheme("Password", Icons.lock, context),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Login",
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Theme.of(context).colorScheme.onPrimary),
+                    ),
+                  ),
+                                  SizedBox(
+                    height: 8,
+                  ),
+                  const GoogleSignInButton(),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account?"),
+                      TextButton(onPressed: () {}, child: Text("Sign up"))
+                    ],
+                  )
+                ],
               ),
-              TextField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  filled: true,
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-              ),
-              MaterialButton(
-                color: Colors.red,
-                // backgroundColor: WidgetStateProperty.all(Colors.white),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40),
-                ),
-                onPressed: (){},
-                child: const Text("Login"),
-              ),
-              const GoogleSignInButton(),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     Text("Don't have an account?"),
-              //     TextButton(
-              //       onPressed: (){}, 
-              //       child: Text("Sign up")
-              //     )
-              //   ],
-              // )
-            ],
+            ),
           ),
         ),
       ),
--- lib/utilities/theme.dart
+++ lib/utilities/theme.dart
@@ -1,7 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData trackCashTheme() {
+  ColorScheme colorSchemeTemp = ColorScheme.fromSeed(seedColor: Colors.cyan);
   return ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    colorScheme: colorSchemeTemp,
+    textTheme: TextTheme(
+        headlineLarge: TextStyle(
+          color: colorSchemeTemp.onSurface,
+          fontSize: 30,
+          fontFamily: GoogleFonts.roboto().fontFamily,
+          fontWeight: FontWeight.w600,
+        ),
+        headlineSmall: TextStyle(
+          color: colorSchemeTemp.onSurface,
+          fontSize: 18,
+          fontFamily: GoogleFonts.roboto().fontFamily,
+          fontWeight: FontWeight.w500,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 18,
+          fontFamily: GoogleFonts.roboto().fontFamily,
+          fontWeight: FontWeight.w400,
+        ),
+        bodyLarge: TextStyle(
+          fontSize: 20,
+          fontFamily: GoogleFonts.roboto().fontFamily,
+          fontWeight: FontWeight.w600,
+        )),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: colorSchemeTemp.primary,
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(vertical: 8))),
   );
+}
+
+InputDecoration textFieldTheme(
+    String label, IconData icon, BuildContext context) {
+  return InputDecoration(
+      contentPadding: EdgeInsets.symmetric(horizontal: 16),
+      labelText: label,
+      labelStyle: Theme.of(context)
+          .textTheme
+          .bodyMedium
+          ?.copyWith(color: Theme.of(context).colorScheme.onSurface),
+      floatingLabelStyle: Theme.of(context)
+          .textTheme
+          .bodyMedium
+          ?.copyWith(color: Theme.of(context).colorScheme.primary),
+      border: OutlineInputBorder(
+          borderSide: BorderSide.none, borderRadius: BorderRadius.circular(16)),
+      filled: true,
+      fillColor: Theme.of(context).colorScheme.surfaceContainer,
+      prefixIcon: Icon(
+        icon,
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
+      focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary, width: 1.5)
+        ),
+        );
+      
 }
